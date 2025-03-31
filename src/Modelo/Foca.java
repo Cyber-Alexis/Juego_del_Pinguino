70f46668a353017c03feb8eb9fab6b68795307d5
Deivid <deividsonic2@gmail.com>
2025-03-31 20:08:57 +0200

    Clase Foca
    Poner setters y getters

---
+++ src/Modelo/Foca.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+	public class Foca {
+	    private boolean interactuarConJugador;
+
+	    public boolean isInteractuarConJugador() {
+	        return interactuarConJugador;
+	    }
+
+	    public void setInteractuarConJugador(boolean interactuarConJugador) {
+	        this.interactuarConJugador = interactuarConJugador;
+	    }
+	}
+
+
