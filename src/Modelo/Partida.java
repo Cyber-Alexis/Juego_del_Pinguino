0a4aef7ffb4fac6bd80fbb9fed244cd6d6db9ca1
Alexis Buza Craiu <alexis.n.buza@alumnes.ilerna.com>
2025-03-31 19:50:59 +0200

    Clase de Partida
    En esta clase lo que he hecho ha sido agregar setters

--- src/Modelo/Partida.java
+++ src/Modelo/Partida.java
@@ -1,10 +1,32 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Partida {
+	
+ private Tablero tablero;
+ private List<Jugador> jugadores;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ public Partida() {
+     this.tablero = new Tablero();
+     this.jugadores = new ArrayList<>();
+ }
 
-	}
+ public Tablero getTablero() {
+     return tablero;
+ }
 
+ public void setTablero(Tablero tablero) {
+     this.tablero = tablero;
+ }
+
+ public List<Jugador> getJugadores() {
+     return jugadores;
+ }
+
+ public void setJugadores(List<Jugador> jugadores) {
+     this.jugadores = jugadores;
+     
+ }
+ 
 }
