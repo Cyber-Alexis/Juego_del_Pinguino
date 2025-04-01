044c8415b165f729c314e10b13879c5737bcfefe
Alexis Buza Craiu <alexis.n.buza@alumnes.ilerna.com>
2025-04-01 16:13:08 +0200

    Clase de CasillaPinguino
    En esta clase lo que he puesto ha sido los private string de nombre y color, luego los getters y setters los he puesto en nombre y color

--- src/Modelo/CasillaPinguino.java
+++ src/Modelo/CasillaPinguino.java
@@ -1,10 +1,27 @@
 package Modelo;
 
 public class CasillaPinguino {
+    private String nombre;
+    private String color;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public CasillaPinguino(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
 
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
