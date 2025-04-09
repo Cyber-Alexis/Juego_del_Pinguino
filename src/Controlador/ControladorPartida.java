abaa1570f9654809aba0945f92290e7521d62882
Alexis Buza Craiu <alexis.n.buza@alumnes.ilerna.com>
2025-04-09 17:34:49 +0200

    Controlador de Partida
    En este apartado he puesto lo del controlador de partida he puesto el agregar jugador , el iniciar partida y el turno de jugador

--- src/Controlador/ControladorPartida.java
+++ src/Controlador/ControladorPartida.java
@@ -1,18 +1,33 @@
 package Controlador;
 
-class ControladorPartida {
+import Modelo.Pinguino;
+import Modelo.Partida;
+import java.util.List;
 
-	    private Partida partida;
-	    
-	    public ControladorPartida(Partida partida) {
-	        this.partida = partida;
-	    }
-	    
-	    public int obtenerNumeroJugadoresRegistrados() {
-	        return partida.getNumeroJugadores();
-	    }
-	    
-	    public int obtenerNumeroCasillasGeneradas() {
-	        return partida.getTablero().getNumeroCasillas();
-	    }
+public class ControladorPartida {
+    private Partida partida;
+
+    public ControladorPartida() {
+        this.partida = new Partida();
+    }
+
+    public void agregarJugador(String nombre, String color) {
+        partida.getJugadores().add(new Jugador(nombre, color));
+        System.out.println("Jugador " + nombre + " añadido con color " + color);
+    }
+
+    public void iniciarPartida() {
+        System.out.println("¡La partida ha comenzado!");
+    }
+
+    public void turnoJugador(Jugador jugador, int dado) {
+        System.out.println("Turno de " + jugador.getNombre());
+        int nuevaPosicion = jugador.getPosicion() + dado;
+        System.out.println(jugador.getNombre() + " lanza el dado y avanza " + dado + " casillas.");
+        partida.getTablero().moverJugador(jugador, nuevaPosicion);
+    }
+
+    public List<Jugador> obtenerJugadores() {
+        return partida.getJugadores();
+    }
 }
