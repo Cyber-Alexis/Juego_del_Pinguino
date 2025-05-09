85d6ef0dd94ddf2801e84fd753a021c36d26a981
Alexis Buza Craiu <alexis.n.buza@alumnes.ilerna.com>
2025-05-09 17:01:02 +0200

    Cambios en el Codigo

---
+++ src/Controlador/saveCon.java
@@ -0,0 +1,18 @@
+package Controlador;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class saveCon {
+    
+    private static Connection Conexion;
+
+    public static Connection getConexion() {
+        return Conexion;
+    }
+
+    public static void setConexion(Connection conexion) {
+        Conexion = conexion;
+    }
+    
+}
